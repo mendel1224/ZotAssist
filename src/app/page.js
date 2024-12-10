4114dfcad792d56b9adf1c51eee566b09cf60cc5
mendel1224 <75659972+mendel1224@users.noreply.github.com>
2024-12-09 16:57:12 -0800

    Cleaned up file and folder structure

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from './components/Header.js'
+import Header from './components/Header/Header.js'
 import { Box } from '@mui/material';  
 
 export default function Home() {
