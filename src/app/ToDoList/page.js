e6631388e554c16a82bfba1d54072f45648b1b09
Error508 <35969673+Error508@users.noreply.github.com>
2024-12-10 14:13:06 -0800

    Fixed UI elements and errors for ToDoList

--- src/app/ToDoList/page.js
+++ src/app/ToDoList/page.js
@@ -14,43 +14,44 @@ export default function ToDoList(){
 
     return (
         <Box>
-            <Grid2
-                container
-                spacing = {2}>
-                <Grid2
-                    item
-                    xs={6}>
-                    <TextField
-                        id='outlined-basic'
-                        label='Outlined'
-                        variant='outlined'
-                        value={task}
-                        fullWidth
-                        onChange={(e) => setTask(e.target.value)}
-                    />
+            <Grid2 container spacing = {2}>
+                    <Grid2
+                        size={6}>
+                        <TextField
+                            id='outlined-basic'
+                            label='Type New Task'
+                            variant='outlined'
+                            value={task}
+                            sx={{width: '100%'}}
+                            onChange={(e) => setTask(e.target.value)}
+                        />
+                    
                     </Grid2>
-                        <Grid2 
-                        item xs={6}
-                        fullWidth
+
+                    <Grid2 
+                        size={6}
                         >
-                    <Button 
-                        variant ='contained'
-                        fullWidth
-                        sx={{
-                            height:'100%',
-                        }}
-                        onClick={handleAdd}
-                     > 
-                        Add Task
+                    
+                        <Button 
+                            variant ='contained'
+                            sx={{
+                                height:'100%', width: '100%'
+                            }}
+                            onClick={handleAdd}
+                        > 
+                            Add Task
                         </Button>
+
                     </Grid2>
-                    <Grid2 item xs={12}>
+
+                    <Grid2 size={12}>
                         <ul>
                             {tasks.map((task, index) => (
-                                <li key ={index}>{todo}</li>
+                                <li key ={index}>{task}</li>
                             ))}
                         </ul>
                     </Grid2>
+
                 </Grid2>
         </Box>
     )
