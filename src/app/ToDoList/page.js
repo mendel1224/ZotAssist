8a0e6439a2c1c8e67ef2ef99c27eeed09ff6eb13
Error508 <35969673+Error508@users.noreply.github.com>
2024-12-11 18:14:12 -0800

    commented and cleaned up code

--- src/app/ToDoList/page.js
+++ src/app/ToDoList/page.js
@@ -5,24 +5,24 @@ import {useState} from 'react';
 import { useEffect } from 'react';
 
 export default function ToDoList(){
+    //Setting up variables and list for TodoList
     const [tasks, setTasks] = useState(() => {
 
         const saved = localStorage.getItem("todolist") || "[]";
         const initialValue = JSON.parse(saved)
         return initialValue || "";
+        //Fetching list data from localstorage
 
     });
     const [task, setTask] = useState('');
     const [isEdited, setIsEdited] = useState(false);
     const [editedId, setEditedId] = useState(null);
-
-    const inputChange = (e) => {
-        setTask(e.target.value);
-    };
+    //Variables showing if task has been edited/has not
 
     useEffect(() =>{
         localStorage.setItem("todolist", JSON.stringify(tasks));
         }, [tasks]);
+    //Stores tasks in localstorage
 
     const handleAction = () => {
         if(!isEdited){
@@ -34,10 +34,12 @@ export default function ToDoList(){
         setTask("");
         setIsEdited(false);
     };
+    //Handles when Add task or Edit button is pressed depending on which it is, then clears
 
     const handleDelete = (id) => {
         setTasks((prev) => prev.filter((task) => task.id !== id));
     };
+    //Deletes a task from the list
 
     const handleDone = (id) => {
         const updated = tasks.map((task) => {
@@ -48,6 +50,7 @@ export default function ToDoList(){
           });
           setTasks(updated);
     }
+    //Updates when task is marked as finished (or changed to unfinished)
 
     const handleEdit = (id) => {
         const newTasks = tasks.filter((task) => task.id !== id);
@@ -57,7 +60,9 @@ export default function ToDoList(){
         setTasks(newTasks);
         setIsEdited(true);
     };
+    //Updates a task when edited
 
+    //Display code, used box with grid2 primarily to organize
     return (
         <Box>
             <Grid2 container spacing = {2}>
@@ -85,9 +90,11 @@ export default function ToDoList(){
                             }}
                             onClick={handleAction}
                             disabled={task ? false : true}
+                            //Button disables itself if there is input typed into the text field, if not makes itself pushable
                         > 
                             {isEdited ? "Edit" : "Add Task"}
                         </Button>
+                        {/* When a task is being edited, changes the add task button to edit, if not, then change back to add Task */}
 
                     </Grid2>
 
@@ -111,6 +118,7 @@ export default function ToDoList(){
                                     </ListItem>
                                 );
                             })}
+                            {/* Handles displaying the list, with buttons handling edit, delete functions and a checkbox handling done */}
                         </List>
                     </Grid2>
 
